0a5c4cba7a3647b102f237795117d5c9c1e1f0dd
senderro <132297098+senderro@users.noreply.github.com>
2025-07-22 23:31:25 -0300

    att final

--- index.js
+++ index.js
@@ -413,8 +413,8 @@ msgText += `\n*Gasto total:* R$ ${total.toFixed(2)}`;
       await prisma.pendingPayment.create({
         data: {
           bag_id:         bag.id,
-          user_id_from:   BigInt(t.para),
-          user_id_to:     BigInt(t.de),
+          user_id_from:   BigInt(t.de),
+          user_id_to:     BigInt(t.para),
           valor:          t.valor,
           pago:           false,
           data_pagamento: null,
